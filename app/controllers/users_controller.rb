9334a59258cb7779394373ed8c6e316802a27a6d
Sharmaine Requerme <srqrme@gmail.com>
2019-02-15 18:37:52 +0000

    update users controller with logout route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,4 +36,12 @@ class UsersController < ApplicationController
     end
   end
 
+  get '/logout' do
+    if logged_in?
+      session.destroy
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end
 end
