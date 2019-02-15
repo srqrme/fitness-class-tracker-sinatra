d8dca07edc5ef638715dd234f9950ffa9feca03f
Sharmaine Requerme <srqrme@gmail.com>
2019-02-15 18:04:49 +0000

    update application controller route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,11 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
-    erb :welcome
+    if logged_in?
+      redirect to '/fitness_classes'
+    else
+      erb :welcome
+    end
   end
 
   helpers do
