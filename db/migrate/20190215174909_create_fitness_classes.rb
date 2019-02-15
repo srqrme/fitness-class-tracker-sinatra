3500aabd0cb35367118a8c030b367f142fccbc86
Sharmaine Requerme <srqrme@gmail.com>
2019-02-15 17:53:18 +0000

    create fitness classes table

--- db/migrate/20190215174909_create_fitness_classes.rb
+++ db/migrate/20190215174909_create_fitness_classes.rb
@@ -1,4 +1,12 @@
 class CreateFitnessClasses < ActiveRecord::Migration
   def change
+    create_table :fitness_classes do |t|
+      t.string :name
+      t.date :date
+      t.time :time
+      t.string :location
+      t.string :instructor
+      t.integer :user_id
+    end
   end
 end
