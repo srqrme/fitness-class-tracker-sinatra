3aea00b84f6bc4d3ade2d1da473826300e495d5b
Sharmaine Requerme <srqrme@gmail.com>
2019-02-15 18:26:39 +0000

    update users controller post signup route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,14 @@ class UsersController < ApplicationController
     end
   end
 
+  post '/signup' do
+    if params[:name] == "" || params[:email] == "" || params[:username] == "" || params[:password] == ""
+      redirect to "/signup"
+    else
+      @user = User.create(:name => params[:name], :email => params[:email], :username => params[:username], :password => params[:password])
+      session[:user_id] = @user.id
+      redirect to '/fitness_classes'
+    end
+  end
+
 end
