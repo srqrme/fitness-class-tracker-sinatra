cc8977994b5a726fdcd1bca43e9522e0b19c9559
Sharmaine Requerme <srqrme@gmail.com>
2019-02-15 18:45:33 +0000

    update fitness classes controller with fitness_classes route

--- app/controllers/fitness_classes_controller.rb
+++ app/controllers/fitness_classes_controller.rb
@@ -1,3 +1,12 @@
 class FitnessClassesController < ApplicationController
 
+  get '/fitness_classes' do
+    if logged_in?
+      @fitness_classes = current_user.fitness_classes.all
+      erb :'fitness_classes/classes'
+    else
+      redirect to '/login'
+    end
+  end
+
 end
