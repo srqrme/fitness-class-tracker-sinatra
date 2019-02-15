a9eefce79357b46e819878fb1dd1e38651983130
Sharmaine Requerme <srqrme@gmail.com>
2019-02-15 19:10:39 +0000

    update fitness controller with get fitness_classes/:id route

--- app/controllers/fitness_classes_controller.rb
+++ app/controllers/fitness_classes_controller.rb
@@ -33,4 +33,17 @@ class FitnessClassesController < ApplicationController
       redirect to "/fitness_classes/#{@fitness_class.id}"
     end
   end
+
+  get '/fitness_classes/:id' do
+    if logged_in?
+      @fitness_class = FitnessClass.find_by_id(params[:id])
+      if @fitness_class.user_id == current_user.id
+        erb :'/fitness_classes/show'
+      else
+        redirect to "/fitness_classes"
+      end
+    else
+      redirect to '/login'
+    end
+  end
 end
