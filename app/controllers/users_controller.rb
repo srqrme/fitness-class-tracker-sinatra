7e5437c4c5401a3a2aed6ba517650ca5ea3b868a
Sharmaine Requerme <srqrme@gmail.com>
2019-02-15 18:36:53 +0000

    update users controller with post login route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,4 +25,15 @@ class UsersController < ApplicationController
       erb :'users/login'
     end
   end
+
+  post '/login' do
+    @user = User.find_by(:username => params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to '/fitness_classes'
+    else
+      redirect to '/login'
+    end
+  end
+
 end
