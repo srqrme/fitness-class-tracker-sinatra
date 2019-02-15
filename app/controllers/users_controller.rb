db28f4d61934088d11e836e3fdeca29d2c0db591
Sharmaine Requerme <srqrme@gmail.com>
2019-02-15 18:12:54 +0000

    update users controller with signup route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
 
-
+  get '/signup' do
+    if !logged_in?
+      erb :'users/signup'
+    else
+      redirect to '/fitness_classes'
+    end
+  end
 
 end
