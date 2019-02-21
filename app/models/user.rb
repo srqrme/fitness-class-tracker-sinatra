e0eb8091568a2a19686188cc904de3a59ba35214
Sharmaine Requerme <srqrme@gmail.com>
2019-02-21 21:56:27 +0000

    update files

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   has_many :fitness_classes
   has_secure_password
 
+  #validates_uniqueness_of :username, :case_sensitive => false, :on => :create, message: "is already taken"
+
 end
